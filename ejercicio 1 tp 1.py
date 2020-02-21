feb1d86be7631cf6a8762e23539c99a4a0fbdbfe
QUIMEY <56846039+quimeyruffa@users.noreply.github.com>
2020-02-21 18:00:48 -0300

    Update ejercicio 1 tp 1.py

--- ejercicio 1 tp 1.py
+++ ejercicio 1 tp 1.py
@@ -14,15 +14,15 @@ def MayorEstricto(a,b,c):
                 mayor=c
     return mayor
 num=int(input('Ingrese un numero: '))
-while num<0:
+while num<=0:
     print('El numero debe ser positivo')
     num=int(input('Ingrese un numero: '))
 num1=int(input('Ingrese un numero: '))
-while num1<0:
+while num1<=0:
     print('El numero debe ser positivo')
     num1=int(input('Ingrese un numero: '))
 num2=int(input('Ingrese un numero: '))
-while num2<0:
+while num2<=0:
     print('El numero debe ser positivo')
     num2=int(input('Ingrese un numero: '))
 funcion=MayorEstricto(num,num1,num2)
